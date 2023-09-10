12723edb576c8e41365fb13c46fb8cf2db6c5359
GermanPachec0 <87890408+GermanPachec0@users.noreply.github.com>
2023-09-10 12:40:15 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import './App.css';
+
+function App() {
+    const [chapters, setChapters] = useState([]);
+    const [chapter, setChapter] = useState({ pages: [] }); // Initialize chapter with pages property
+    const [selectedOption, setSelectedOption] = useState(useState());
+
+    useEffect(() => {
+        const fetchChapters = async () => {
+            try {
+                const response = await axios.get('http://localhost:1234/chapters');
+                if (response.data) {
+                    setChapters(response.data);
+                }
+            } catch (error) {
+                console.error('Error fetching chapters:', error);
+            }
+        };
+
+        fetchChapters();
+    }, []);
+
+const fetchChapterDetails = async (chapterId) => {
+    try {
+        const response = await axios.get(`http://localhost:1234/chapter_details/${chapterId}`);
+        if (response.data) {
+            setChapter(response.data);
+        }
+    } catch (error) {
+        console.error('Error fetching chapter details:', error);
+    }
+};
+
+const handleSelectChange = (event) => {
+    setSelectedOption(event.target.value);
+    fetchChapterDetails(event.target.value);
+};
+    console.log(chapters);
+
+    return (
+  <div>
+      <div className="select-container">
+        <label htmlFor="chapter-select">Select a chapter:</label>
+        <select
+          id="chapter-select"
+          value={selectedOption}
+          onChange={handleSelectChange}
+        >
+          {chapters.map((option) => (
+            <option key={option.text} value={option.value}>
+              {option.text}
+            </option>
+          ))}
+        </select>
+      </div>
+            <Carousel 
+                showThumbs={true}
+                showStatus={true}
+            >
+                {chapter.pages.map((page) => (
+                    <div key={page.page}>
+                        <img src={page.img} alt={`Page ${page.value}`} />
+                    </div>
+                ))}
+            </Carousel>
+    </div>
+    );
+}
+
+export default App;
