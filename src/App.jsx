9c2d4f9e5e03572c653ef7aa4360cd0d670075d8
germanpachec0 <germanpacheco98@gmail.com>
2024-08-18 17:43:53 -0300

    test in vercel app

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,36 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Carousel } from 'react-responsive-carousel';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; 
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "./App.css";
 
 function App() {
-    const [chapters, setChapters] = useState([]);
-    const [chapter, setChapter] = useState({ pages: [] });
-    const [selectedOption, setSelectedOption] = useState();
+  const [selectedOption, setSelectedOption] = useState();
 
-    const fetchChapters = async () => {
-            try {
-                const response = await axios.get('http://localhost:1234/chapters');
-                if (response.data) {
-                    setChapters(response.data);
+  const handleSelectChange = (event) => {
+    setSelectedOption(event.target.value);
+    fetchChapterDetails(event.target.value);
+  };
 
-                }
-            } catch (error) {
-                console.error('Error fetching chapters:', error);
-            }
-        };
- 
-    const fetchChapterDetails = async (chapterId) => {
-        try {
-            const response = await axios.get(`http://localhost:1234/chapter_details/${chapterId}`);
-            if (response.data) {
-                setChapter(response.data);
-            }
-        } catch (error) {
-            console.error('Error fetching chapter details:', error);
-        }
-    };
-
-    const handleSelectChange = (event) => {
-        setSelectedOption(event.target.value);
-        fetchChapterDetails(event.target.value);
-    };
-
-    useEffect(() =>{fetchChapters()},[])
-
-    return (
+  return (
+    <div>
+      <div className="select-container">
+        <label htmlFor="chapter-select">Select a chapter: </label>
+        <option defaultValue="01"></option>
+        <select
+          id="chapter-select"
+          value={selectedOption}
+          onChange={handleSelectChange}
+        ></select>
+      </div>
+      <br></br>
+      <Carousel showThumbs={true} showStatus={true}>
         <div>
-            <div className="select-container">
-                <label htmlFor="chapter-select">Select a chapter:   </label>
-                <option defaultValue='01'></option>
-                <select
-                    id="chapter-select"
-                    value={selectedOption}
-                    onChange={handleSelectChange}
-                >
-                    {chapters.map((option) => (
-                        <option key={option.text} value={option.value}>
-                            {option.text}
-                        </option>
-                    ))}
-                </select>
-            </div>
-            <br></br>
-            <Carousel 
-                showThumbs={true}
-                showStatus={true}
-            >
-                {chapter.pages.map((page) => (
-                    <div key={page.page}>
-                        <img src={page.img} alt={`Page ${page.value}`} />
-                    </div>
-                ))}
-            </Carousel>
+          <img src="https://uploads.mangadex.org/data/3303dd03ac8d27452cce3f2a882e94b2/1-f7a76de10d346de7ba01786762ebbedc666b412ad0d4b73baa330a2a392dbcdd.png" />
         </div>
-    );
+      </Carousel>
+    </div>
+  );
 }
 
 export default App;
